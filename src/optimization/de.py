fe4c4a9046759e921d611ac03efcdc5b960f44b4
Victor Ruela <victorspruela@gmail.com>
2020-10-07 21:23:25 -0300

    v0 do algoritmo DE

--- src/optimization/de.py
+++ src/optimization/de.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class DifferentialEvolution:
-    def __init__(self, func, ub, lb, mutation_prob=0.5, pop_size=50, max_generations=100, type='rand/1/bin'):
+    def __init__(self, func, ub, lb, mutation_prob=0.5, pop_size=50, max_generations=100, de_type='rand/1/bin'):
         self.fobj = func
         self.lb = lb
         self.ub = ub
@@ -11,13 +11,13 @@ class DifferentialEvolution:
         self.num_variables = len(lb)
         self.mutation_prob = mutation_prob
 
-        self.base, self.d, self.rec = type.split('/')
+        self.base, self.d, self.rec = de_type.split('/')
 
         self.reset()
 
     def reset(self):
-        self.cost_evals = 0
-        self.population_cost = []
+        self.fobj_evals = 0
+        self.population_fobj = []
         self.best_objective = np.Infinity
         self.best_solution = []
 
@@ -28,22 +28,26 @@ class DifferentialEvolution:
         self.initial_population = np.copy(self.population)
 
     def evaluate_population_cost(self, population):
-        pop_cost = []
+        pop_fobj = []
+        pop_g = []
         # Calculating the fitness value of each solution in the current population.
         for sol in population:
-            cost = self.fobj(sol)
+            cost, g = self.eval_objective(sol)
 
             if(cost > self.best_objective):
                 self.best_objective = cost
                 self.best_solution = sol
 
-            pop_cost.append(cost)
+            pop_fobj.append(cost)
+            pop_g.append(g)
 
-        pop_cost = np.array(pop_cost)
+        pop_fobj = np.array(pop_fobj)
 
-        self.cost_evals = self.cost_evals + pop_cost.shape[0]
-        self.population_cost = pop_cost
-        return pop_cost
+        self.fobj_evals = self.fobj_evals + pop_fobj.shape[0]
+        self.population_fobj = pop_fobj
+        self.population_g = np.asarray(pop_g)
+
+        return pop_fobj
 
     def select_base_vector(self):
         if(self.base == 'rand'):
@@ -68,7 +72,7 @@ class DifferentialEvolution:
                 'd={} is not implemented!'.format(self.d))
 
     def select_scale_factor(self):
-        return 0.5
+        return np.random.rand() * 0.5 + 0.5  # U(0.5, 1.0)
 
     def mutate(self, target, scale_factor, difference):
         return target + scale_factor * difference
@@ -91,16 +95,48 @@ class DifferentialEvolution:
             raise ValueError(
                 'Recombination={} is not implemented!'.format(self.rec))
 
-    def select_survivors(self, u):
-        return u
+    def validate_bounds(self, x):
+        xc = []
+        for i, value in enumerate(x):
+            if((value < self.lb[i]) | (value > self.ub[i])):
+                # replace the variable by a random value inside the bounds
+                xc.append(np.random.rand() *
+                          (self.ub[i] - self.lb[i]) + self.lb[i])
+            else:
+                xc.append(value)
+
+        return np.asarray(xc)
+
+    def eval_objective(self, x):
+        # truncate x
+        cost, g = self.fobj(x)
+
+        # handle constraints with penalty method
+        g = np.maximum(g, np.zeros_like(g))
+        cost = cost + 1000 * np.sum(g)
+
+        return cost, g
+
+    def select_survivors(self, u, x, fx):
+        survivors = []
+        for i in range(self.population_size):
+            u_i = self.validate_bounds(u[i])
+            fu = self.eval_objective(u_i)
+            # TODO: implement the constraint handling from Lampinen 2002 here
+            if(fu <= fx[i]):
+                survivors.append(u_i)
+            else:
+                survivors.append(x[i])
+
+        return np.asarray(survivors)
 
     def run(self, debug=True):
         self.initialize_population()
-        for generation in range(self.max_generations):
+        for _ in range(self.max_generations):
             fitness = self.evaluate_population_cost(self.population)
             v = []
 
-            for i in range(self.population_size):
+            for _ in range(self.population_size):
                 r1, base = self.select_base_vector()
                 difference = self.select_difference_vector(r1)
                 scale_factor = self.select_scale_factor()
@@ -108,6 +144,7 @@ class DifferentialEvolution:
 
             v = np.asarray(v)
             u = self.recombine(v, self.population)
-            survivors = self.select_survivors(u)
+            self.population = self.select_survivors(
+                u, self.population, fitness)
 
-        return self.best_cost, self.best_solution
+        return self.best_objective, self.best_solution
