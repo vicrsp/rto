b35ab39361b4359e8952fcb9888624f59503928a
Victor Ruela <victorspruela@gmail.com>
2021-05-18 09:00:18 -0300

    - implementando sistema de wells

--- src/model/process/gas_lifted_wells.py
+++ src/model/process/gas_lifted_wells.py
@@ -40,45 +40,35 @@ GOR = 0.1 # [kg/kg]
 mu_oil = 0.001 # 1cP
 
 class GasLiftedWell:
-    def __init__(self, y0=[1, 1, 1]):
+    def __init__(self, name, k=(2.2, 0.1, 900), y0=[1, 1, 1]):
+      self.name = name
       self.y0 = y0
-      self.stoptime = 300 * 60
+      self.PI, self.GOR, self.rho_o = k
+      self.stoptime = 300
       self.numpoints = 100
-      self.sim_results = []
       
-    def odecallback(self, t, x, u):
+    def calculate_flows(self, x):
       m_ga, m_gt, m_ot = x # current state
-      w_gl = u[0] # current inputs
-
-      # Intermediates
-      # p_a = 1e-5*((m_ga*1e3) * ((Ta*R/(Va*Mw) + g*La/(Aa*La))))
-      # p_wh = 1e-5*((Tw*R/Mw)*(m_gt*1e3)/(Lw*Aw + Lr*Ar - ((m_ot*1e3)/rho_o)))
-      # p_wi = 1e-5*(p_wh*1e5 + (g/(Aw*Lw))*max(0,m_ot*1e3 + m_gt*1e3 - rho_o*Lr*Ar)*Hw)
-      # p_bh = 1e-5*(p_wi*1e5 + rho_o*g*Hr)
       
-      # rho_a = 1e-2*(Mw * (p_a*1e5) / (Ta * R))
-      # rho_m = 1e-2*(m_gt*1e3 + m_ot*1e3 - rho_o * Lr * Ar)/(Lw*Aw)
-      
-      # w_pc = C_pc*np.sqrt((rho_m*1e2)*max(0, p_wh*1e5 - p_m*1e5))
-      # w_ro = (PI*1e-6)*(p_r*1e5 - p_bh*1e5)
-      # w_rg = 1e1*GOR*w_ro
-      # w_iv = C_iv*np.sqrt((rho_a*1e2)*max(0, p_a*1e5 - p_wi*1e5))
-
-      # w_pg = (m_gt*1e3)*w_pc/(m_gt*1e3 + m_ot*1e3)
-      # w_po = (m_ot*1e3)*w_pc/(m_gt*1e3 + m_ot*1e3)
       p_ai = 1e-5*(((R*T_a/(V_a*Mw) + g*H_a/V_a)*m_ga*1e3) + (Mw/(R*T_a)*((R*T_a/(V_a*Mw) + g*H_a/V_a)*m_ga*1e3))*g*H_a)
-      p_wh = 1e-5*(((R*T_w/Mw)*(m_gt*1e3/(L_w*A_w + L_bh*A_bh - m_ot*1e3/rho_o))) - ((m_gt*1e3+m_ot*1e3 )/(L_w*A_w))*g*H_w/2)
+      p_wh = 1e-5*(((R*T_w/Mw)*(m_gt*1e3/(L_w*A_w + L_bh*A_bh - m_ot*1e3/self.rho_o))) - ((m_gt*1e3+m_ot*1e3 )/(L_w*A_w))*g*H_w/2)
       rho_ai = 1e-2*(Mw/(R*T_a)*p_ai*1e5)
-      rho_m = 1e-2*(((m_gt*1e3 + m_ot*1e3)*p_wh*1e5*Mw*rho_o)/(m_ot*1e3*p_wh*1e5*Mw + rho_o*R*T_w*m_gt*1e3)) 
+      rho_m = 1e-2*(((m_gt*1e3 + m_ot*1e3)*p_wh*1e5*Mw*self.rho_o)/(m_ot*1e3*p_wh*1e5*Mw + rho_o*R*T_w*m_gt*1e3)) 
       w_pc = C_pc*math.sqrt(max(0, rho_m*1e2*(p_wh*1e5 - p_m*1e5)))
       w_pg = (m_gt*1e3/(m_gt*1e3+m_ot*1e3))*w_pc
       w_po = (m_ot*1e3/(m_gt*1e3+m_ot*1e3))*w_pc
-      p_wi = 1e-5*((p_wh*1e5 + g/(A_w*L_w)*max(0,(m_ot*1e3+m_gt*1e3-rho_o*L_bh*A_bh))*H_w + 128*mu_oil*L_w*w_pc/(3.14*(D_w**4)*((m_gt*1e3 + m_ot*1e3)*p_wh*1e5*Mw*rho_o)/(m_ot*1e3*p_wh*1e5*Mw + rho_o*R*T_w*m_gt*1e3))))
-      p_bh = 1e-5*(p_wi*1e5 + rho_o*g*H_bh + 128*mu_oil*L_bh*w_po/(math.pi*(D_bh**4)*rho_o))
+      p_wi = 1e-5*((p_wh*1e5 + g/(A_w*L_w)*max(0,(m_ot*1e3+m_gt*1e3-self.rho_o*L_bh*A_bh))*H_w + 128*mu_oil*L_w*w_pc/(3.14*(D_w**4)*((m_gt*1e3 + m_ot*1e3)*p_wh*1e5*Mw*self.rho_o)/(m_ot*1e3*p_wh*1e5*Mw + self.rho_o*R*T_w*m_gt*1e3))))
+      p_bh = 1e-5*(p_wi*1e5 + self.rho_o*g*H_bh + 128*mu_oil*L_bh*w_po/(math.pi*(D_bh**4)*self.rho_o))
       w_iv = C_iv*math.sqrt(max(0,rho_ai*1e2*(p_ai*1e5 - p_wi*1e5)))
-      w_ro = (PI)*1e-6*(p_res*1e5 - p_bh*1e5)
-      w_rg = 1e1*GOR*w_ro
+      w_ro = (self.PI)*1e-6*(p_res*1e5 - p_bh*1e5)
+      w_rg = 1e1*self.GOR*w_ro
 
+      return w_pg,w_po,w_iv,w_ro,w_rg
+
+    def odecallback(self, t, x, u):
+      w_gl = float(u) # current inputs
+      # Flows
+      w_pg,w_po,w_iv,w_ro,w_rg=self.calculate_flows(x)
       # Process model
       df = [
           (w_gl - w_iv)*1e-3,
@@ -91,25 +81,95 @@ class GasLiftedWell:
       self.sim_results = []
       t = [self.stoptime * float(i) / (self.numpoints - 1)
             for i in range(self.numpoints)]
-      xnew = [u]         
+      xnew = u        
       return solve_ivp(fun=self.odecallback, t_span=[0, self.stoptime], t_eval=t, y0=self.y0, args=(xnew,))
 
     def solve_steady_state(self, u):  
       def fobj(x):
         return self.odecallback(0, x, u)
       return fsolve(func=fobj, x0=self.y0)
+
+
+    # def get_samples(self, u, noise=True):
+    #   x_ss = self.solve_steady_state(u)
+    #   samples = self.calculate_flows(x_ss)
+    #   return samples if noise == None else samples * (1 + np.random.normal(scale=noise)) 
+
+    # def get_objective(self, u, noise=None):
+    #     x_ss = self.solve_steady_state(u)
+    #     w_pc,w_pg,w_po,w_iv,w_ro,w_rg=self.calculate_flows(x_ss)
+    #     fx = w_po ** 2 - 0.5*(u[0]**2)
+    #     return fx if noise == None else fx * (1 + np.random.normal(scale=noise))
+
+    # def get_constraints(self, u, noise=None):
+    #     x_ss = self.solve_steady_state(u)
+    #     w_pc,w_pg,w_po,w_iv,w_ro,w_rg=self.calculate_flows(x_ss)
+    #     fx = w_po ** 2 - 0.5*(u[0]**2)
+    #     return fx if noise == None else fx * (1 + np.random.normal(scale=noise))
+
+    # def plot_simulation(self, sim_results):
+    #   for i, result in enumerate(sim_results.y):
+    #     plt.figure(figsize=(8,6))
+    #     plt.plot(sim_results.t, result)
+    #     plt.title(f'Signal {i}')
+
+
+
+config = { 'well1': { 'GOR': 0.1, 'PI': 2.2, 'rho_o': 900 },
+           'well2': { 'GOR': 0.1, 'PI': 2.2, 'rho_o': 900}} 
+
+class GasLiftwedWellSystem:
+  def __init__(self, config, costs=[1.0, 0.3]):
+    self.oil_cost, self.gas_cost = costs
+    self.n_w = len(config.keys())
+    self.wells = [GasLiftedWell(k, v.values()) for k,v in config.items()]
+
+  def get_objective(self, u, noise=None):
+    fx = 0
+    for i, well in enumerate(self.wells):
+      x_ss = well.solve_steady_state(u[i])
+      _,w_po,_,_,_= well.calculate_flows(x_ss)
+      fx += self.oil_cost * w_po - self.gas_cost * u[i]
+
+    return fx if noise == None else fx * (1 + np.random.normal(scale=noise))
+
+  
+  def get_constraints(self, u, noise=None):
+    g = 0
+    for i, well in enumerate(self.wells):
+      x_ss = well.solve_steady_state(u[i])
+      w_pg,_,_,_,_= well.calculate_flows(x_ss)
+      g += w_pg
     
-    def plot_simulation(self, sim_results):
-      for i, result in enumerate(sim_results.y):
-        plt.figure(figsize=(8,6))
-        plt.plot(sim_results.t, result)
-        plt.title(f'Signal {i}')
-        
-        
-gl = GasLiftedWell()
-# sim_results = gl.simulate(1)
-ss = gl.solve_steady_state([1])
-print(ss)
+    if(noise != None):
+      g = g * (1 + np.random.normal(scale=noise))
+    
+    return np.array([g])
+
+gs = GasLiftwedWellSystem(config)
+
+fig, ax = plt.subplots(figsize=(8,6))
+
+u1 = np.linspace(0.5, 5, 100)
+u2 = np.linspace(0.5, 5, 100)
+
+xx, yy = np.meshgrid(u1, u2)
+# flatten each grid to a vector
+r1, r2 = xx.flatten(), yy.flatten()
+r1, r2 = r1.reshape((len(r1), 1)), r2.reshape((len(r2), 1))
+
+# horizontal stack vectors to create x1,x2 input for the model
+grid = np.hstack((r1,r2))
+# make predictions for the grid
+cost = np.array([gs.get_objective(x) for x in grid])
+# reshape the predictions back into a grid
+zz = cost.reshape(xx.shape)
+ax.contour(xx, yy, zz)
+
+# gl = GasLiftedWell('test', 0)
+# # sim_results = gl.simulate(1)
+# ss = gl.get_objective([1])
+# print(ss)
 
 #gl.plot_simulation(sim_results)
 # fig, ax = plt.subplots(3, 1, figsize=(12,8))
